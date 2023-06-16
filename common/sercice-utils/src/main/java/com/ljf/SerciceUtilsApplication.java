067d390f04ca1055527f4927180add57fbacecd0
Administrator <2813119976@qq.com>
2023-06-16 11:18:43 +0800

    项目基础环境搭建

---
+++ common/sercice-utils/src/main/java/com/ljf/SerciceUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.ljf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerciceUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerciceUtilsApplication.class, args);
+    }
+
+}
